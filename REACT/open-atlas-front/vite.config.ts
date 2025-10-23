fe6c8f739336235649ba9735b69db356e0b2b221
splashycoder <splashprobes@gmail.com>
2025-10-22 21:18:25 -0500

    fix: changes in alias

--- REACT/open-atlas-front/vite.config.ts
+++ REACT/open-atlas-front/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   resolve: {
+  resolve: {
     alias: {
-      '@src': path.resolve(__dirname, './src'),
+      '@src': resolve(__dirname, 'src'),
     }
   }
 })
