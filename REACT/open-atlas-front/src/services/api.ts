90a20c51a619ba040c359c590c65a0e84619f5ac
splashycoder <splashprobes@gmail.com>
2025-10-22 21:20:56 -0500

    feat(api): api service created

---
+++ REACT/open-atlas-front/src/services/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import type { TaskwithDetailsType } from '@src/types/task/Task.type';
+const api_url = import.meta.env.VITE_API_URL
+
+export const api = axios.create({
+    baseURL: api_url,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const taskService = {
+  getUserTasks: async (userId: number): Promise<{ tasks: TaskwithDetailsType[] }> => {
+    const response = await api.get(`/task/api/users/${userId}/tasks`);
+    return response.data;
+  },
+};
